5b7155f21f9365f9ce39a92f7a7b451a7fa8c242
Crowdbotics <team@crowdbotics.com>
2023-08-21 10:23:24 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "googlesheetsconnect-42990",
+  url: "https://googlesheetsconnect-42990.botics.co",
+  api: "https://googlesheetsconnect-42990.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
