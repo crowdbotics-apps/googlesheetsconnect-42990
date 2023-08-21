299af67e28bab49b5592f6f373457c400e754fd2
Saad bin Abid <saad.abid@crowdbotics.com>
2023-08-21 16:00:37 +0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- store/googlesheetsconnectAPI/api.js
+++ store/googlesheetsconnectAPI/api.js
@@ -1,61 +1,72 @@
-import axios from "axios"
+import axios from "axios";
 const googlesheetsconnectAPI = axios.create({
   baseURL: "https://googlesheetsconnect-42990.botics.co",
-  headers: { Accept: "application/json", "Content-Type": "application/json" }
-})
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json"
+  }
+});
+
 function api_v1_login_create(payload) {
-  return googlesheetsconnectAPI.post(`/api/v1/login/`, payload.data)
+  return googlesheetsconnectAPI.post(`/api/v1/login/`, payload.data);
 }
+
 function api_v1_signup_create(payload) {
-  return googlesheetsconnectAPI.post(`/api/v1/signup/`, payload.data)
+  return googlesheetsconnectAPI.post(`/api/v1/signup/`, payload.data);
 }
+
 function rest_auth_user_retrieve(payload) {
-  return googlesheetsconnectAPI.get(`/rest-auth/user/`)
+  return googlesheetsconnectAPI.get(`/rest-auth/user/`);
 }
+
 function rest_auth_user_update(payload) {
-  return googlesheetsconnectAPI.put(`/rest-auth/user/`, payload.data)
+  return googlesheetsconnectAPI.put(`/rest-auth/user/`, payload.data);
 }
+
 function rest_auth_user_partial_update(payload) {
-  return googlesheetsconnectAPI.patch(`/rest-auth/user/`, payload.data)
+  return googlesheetsconnectAPI.patch(`/rest-auth/user/`, payload.data);
 }
+
 function api_docs_schema_retrieve(payload) {
   return googlesheetsconnectAPI.get(`/api-docs/schema/`, {
-    params: { lang: payload.lang }
-  })
+    params: {
+      lang: payload.lang
+    }
+  });
 }
+
 function rest_auth_login_create(payload) {
-  return googlesheetsconnectAPI.post(`/rest-auth/login/`, payload.data)
+  return googlesheetsconnectAPI.post(`/rest-auth/login/`, payload.data);
 }
+
 function rest_auth_logout_retrieve(payload) {
-  return googlesheetsconnectAPI.get(`/rest-auth/logout/`)
+  return googlesheetsconnectAPI.get(`/rest-auth/logout/`);
 }
+
 function rest_auth_logout_create(payload) {
-  return googlesheetsconnectAPI.post(`/rest-auth/logout/`)
+  return googlesheetsconnectAPI.post(`/rest-auth/logout/`);
 }
+
 function rest_auth_registration_create(payload) {
-  return googlesheetsconnectAPI.post(`/rest-auth/registration/`, payload.data)
+  return googlesheetsconnectAPI.post(`/rest-auth/registration/`, payload.data);
 }
+
 function rest_auth_password_reset_create(payload) {
-  return googlesheetsconnectAPI.post(`/rest-auth/password/reset/`, payload.data)
+  return googlesheetsconnectAPI.post(`/rest-auth/password/reset/`, payload.data);
 }
+
 function rest_auth_password_change_create(payload) {
-  return googlesheetsconnectAPI.post(
-    `/rest-auth/password/change/`,
-    payload.data
-  )
+  return googlesheetsconnectAPI.post(`/rest-auth/password/change/`, payload.data);
 }
+
 function rest_auth_password_reset_confirm_create(payload) {
-  return googlesheetsconnectAPI.post(
-    `/rest-auth/password/reset/confirm/`,
-    payload.data
-  )
+  return googlesheetsconnectAPI.post(`/rest-auth/password/reset/confirm/`, payload.data);
 }
+
 function rest_auth_registration_verify_email_create(payload) {
-  return googlesheetsconnectAPI.post(
-    `/rest-auth/registration/verify-email/`,
-    payload.data
-  )
+  return googlesheetsconnectAPI.post(`/rest-auth/registration/verify-email/`, payload.data);
 }
+
 export const apiService = {
   api_v1_login_create,
   api_v1_signup_create,
@@ -71,4 +82,4 @@ export const apiService = {
   rest_auth_password_change_create,
   rest_auth_password_reset_confirm_create,
   rest_auth_registration_verify_email_create
-}
+};
