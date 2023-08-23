f92d1a08d58626f16edf8e3727a7cd8312f52c63
Crowdbotics <engineering@crowdbotics.com>
2023-08-23 13:37:38 +0000

    Update store.

--- store/googleSheets/api.js
+++ store/googleSheets/api.js
@@ -1,24 +1,16 @@
-import axios from "axios";
-import { GOOGLE_SHEETS_TOKEN } from "react-native-dotenv";
+import axios from "axios"
+import { GOOGLE_SHEETS_TOKEN } from "react-native-dotenv"
 const googleSheets = axios.create({
   baseURL: "https://sheets.googleapis.com/v4/spreadsheets",
-  maxBodyLength: Infinity,
   headers: {
     Accept: "application/json",
     "Content-Type": "application/json",
     Authorization: `Bearer ${GOOGLE_SHEETS_TOKEN}`
   }
-});
-
+})
 function googlesheets_get_spreadsheetId_read(payload) {
   return googleSheets.get(`/${payload.spreadsheetId}`, {
-    params: {
-      includeGridData: payload.includeGridData,
-      ranges: payload.ranges
-    }
-  });
+    params: { includeGridData: payload.includeGridData, ranges: payload.ranges }
+  })
 }
-
-export const apiService = {
-  googlesheets_get_spreadsheetId_read
-};
+export const apiService = { googlesheets_get_spreadsheetId_read }
